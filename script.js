cf5bd1e3d499a2705148c404572389fd009914b6
Nillud <agadullin.linar@mail.ru>
2023-07-31 14:12:10 +0500

    script form

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const form = document.getElementById('form-demo')
 
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function(toastEl) {
-    return new bootstrap.Toast(toastEl, option)
-})
+// var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+// var toastList = toastElList.map(function(toastEl) {
+//     return new bootstrap.Toast(toastEl, option)
+// })
 
 form.addEventListener('submit', async(e) => {
     e.preventDefault()
     const formElem = e.currentTarget
 
     let formData = new FormData()
-    formData.append('email', formElem.email.value)
-    formData.append('hot_org_name', formElem.organization.value)
-    formData.append('name', formElem.fio.value)
-    formData.append('city', formElem.city.value)
+    formData.append('fio', formElem.fio.value)
+    formData.append('org_name', formElem.org_name.value)
     formData.append('phone', formElem.phone.value)
 
     let response = await fetch('sendmail.php', {
